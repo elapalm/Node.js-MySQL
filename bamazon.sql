b1c0fda50f8c206ee087544fa53385535b0bf3b4
emma lapalm <lillypadcrafts@gmail.com>
2019-04-13 14:19:35 -0700

    work on SQL database

--- bamazon.sql
+++ bamazon.sql
@@ -1,1 +1,17 @@
-
+--To create the database and insure there isn't a duplicate
+DROP DATABASE IF EXISTS bamazon_DB;
+CREATE DATABASE bamazon_DB;
+-- To insure the correct table is being used
+USE bamazon_DB;
+-- To create the table
+CREATE TABLE products(
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price INT default 0,
+  quantity INT default 0,
+  PRIMARY KEY (id)
+);
+-- To add data to the table
+INSERT INTO bamazon_DB (product_name, department_name, price, quantity)
+VALUES ("", "", , ,);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,25 @@
+// MySQL
 
+var  mysql = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "!do@May2009<3",
+    database: "bamazon_DB",
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id" + connection.threadId);
+    afterConnection();
+});
+
+// Inquirer
+var inquirer = require("inquirer");
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  });

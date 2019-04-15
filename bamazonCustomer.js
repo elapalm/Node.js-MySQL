3238eac94517f97f645d0980685390301230a43d
emma lapalm <lillypadcrafts@gmail.com>
2019-04-14 22:07:38 -0700

    WIP customer interface

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,25 +1,46 @@
 // MySQL
 
-var  mysql = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "!do@May2009<3",
-    database: "bamazon_DB",
+var mysql = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "!do@May2009<3",
+  database: "bamazon_DB",
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id" + connection.threadId);
-    afterConnection();
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id" + connection.threadId);
+  afterConnection();
 });
 
 // Inquirer
 var inquirer = require("inquirer");
 inquirer
   .prompt([
-    /* Pass your questions in here */
+    {
+      type: input,
+      name: item_id,
+      message: "Please enter the Id of the item you would like to purchase.",
+      filter: Number
+    },
+    {
+      type: input,
+      name: quaantity,
+      message: "How many would you like to purchase?",
+      filter: Number
+    },
+
   ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
+  .then (function(answer) {
+    var guestQuantity = answer.quantity;
+    var itemID = answer.ID;
+    customerOrder (itemID, guestQuantity);
   });
+
+function customerOrder(ID, neededQty);
+    connection.query("SELECT * FROM products WHERE id =" + ID, function(err, res)
+     
+
+    
+    );
